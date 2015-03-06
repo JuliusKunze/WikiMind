7100d27a30a17a59f6afc93196a1ef751470f5b5
Julius Kunze <juliuskunze@gmail.com>
2015-03-06 02:20:19 +0100

    prepared mind map structure by adding class Node

--- src/main/java/wikimind/Main.kt
+++ src/main/java/wikimind/Main.kt
@@ -1,8 +1,6 @@
 import java.net.*
 import java.io.*
 import org.jsoup.*
-import org.jsoup.nodes.*
-import org.jsoup.select.*
 import org.sweble.wikitext.engine.*
 import org.sweble.wikitext.engine.utils.*
 import org.sweble.wikitext.parser.nodes.*
@@ -17,20 +15,34 @@ fun main(args: Array<String>) {
     val config = DefaultConfigEnWp.generate()
     val engine = WtEngineImpl(config)
     val page = engine.postprocess(PageId(PageTitle.make(config, title), -1), wikiText, null).getPage()
-    println(page.text())
+
+    val summaryParagraphs = page.filterIsInstance(javaClass<WtParagraph>())
+    val sections = page.filterIsInstance(javaClass<WtSection>())
+    val article = Node(title, paragraphs = summaryParagraphs, subNodes = sections.map { it.toNode() })
+    //println(page.text())
+    println(article.toString())
 }
 
-private fun WtNode.text(indentLevel: Int = 0): String = " ".repeat(2 * indentLevel) + when (this) {
+fun WtSection.toNode(): Node = Node(name = (getHeading().single() as WtText).content.trim(),
+        paragraphs = getBody().filterIsInstance(javaClass<WtParagraph>()),
+        subNodes = getBody().filterIsInstance(javaClass<WtSection>()) map { it.toNode() })
+
+fun WtNode.text(): String = when (this) {
     is WtText -> content
     is WtBold -> "*${single().text()}*"
     is WtItalics -> "/${subTextsCommaJoined()}/"
     is WtPageName -> subTextsCommaJoined()
     is WtLinkTitle -> subTextsCommaJoined()
-    is WtParagraph -> map {it.text()}.join("")
+    is WtParagraph -> subTexts().map { if (it.split("\n").count() <= 1) it else it + "\n" }.join("")
+    is WtTagExtension -> "{${this.getName()}}"
     is WtInternalLink -> "${getTitle().text()}[${getTarget().text()}]"
-    else -> "{${javaClass.getSimpleName()}}${map {"\n" + it.text(indentLevel = indentLevel + 1)}.join("")}"
+    else -> {
+        val subText = subTextsNewLineJoinedAndIndented()
+        "{${javaClass.getSimpleName()}}${"\n" + subText}"
+    }
 }
 
+fun WtNode.subTextsNewLineJoinedAndIndented() = subTexts().join("\n").indented()
 fun URL.getText() = BufferedReader(InputStreamReader(openStream())).readText()
 fun URL.getWikipediaContentAsMarkup() = Jsoup.parse(getText()).select("#wpTextbox1").text()
 
@@ -38,4 +50,13 @@ val WtText.content: String get() = getContent()
 fun WtNode.subTexts() = map {it.text()}
 fun WtNode.subTextsCommaJoined() = subTexts().join(", ")
 
-class Article(val title: String, paragraphs: List<WtParagraph>, val subArticles: List<Article>)
+fun String.indented(indentation: String = "\t") = this.split("\n").map { indentation + it }.join("\n")
+
+class Node(val name: String, val paragraphs: List<WtParagraph>, val subNodes: List<Node>) {
+    override fun toString(): String =
+            "= ${name} ="+
+                    "\n" +
+                    "\n" + paragraphs.map { it.text() }.join("\n") +
+                    "\n" +
+                    "\n" + subNodes.map { it.toString() }.join("\n").indented()
+}
